1661e2b09c254935425219e257f8c0e52cbd35b2
Alexandru Loghin <alexandru.loghin@endava.com>
2020-04-13 12:52:37 +0300

    Add async project

---
+++ FileConsumer/FileConsumerAsync/FileConsumerAsync.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileConsumerAsync
+{
+    internal class FileConsumerAsync
+    {
+        private const string processName = "FileConsumerAsync";
+        private static readonly CancellationTokenSource TokenSource = new CancellationTokenSource();
+        private static readonly List<string> filesConsumed = new List<string>();
+        private static readonly Queue<string> filesInQueue = new Queue<string>();
+        private static int Semaphore;
+        private static CountdownEvent TasksCount;
+
+        public FileConsumerAsync(string filePath, int fileToGenerate, int tasksLimit)
+        {
+            Console.WriteLine($"{processName}: I will process {fileToGenerate} with max {tasksLimit} in parallel.");
+
+            Semaphore = tasksLimit;
+            TasksCount = new CountdownEvent(fileToGenerate);
+
+            var watcher = new FileSystemWatcher(filePath)
+            {
+                IncludeSubdirectories = false,
+                NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite,
+                EnableRaisingEvents = true
+            };
+            watcher.Changed += AddNewFileTask;
+        }
+
+        public async Task<List<string>> StartAsync()
+        {
+            return await Proceed();
+        }
+
+        private void AddNewFileTask(object sender, FileSystemEventArgs e)
+        {
+            filesInQueue.Enqueue(e.FullPath);
+        }
+
+        private async Task<List<string>> Proceed()
+        {
+            var token = TokenSource.Token;
+            var tasks = new List<Task>();
+
+            while (!TokenSource.Token.IsCancellationRequested)
+            {
+                if (TasksCount.CurrentCount.Equals(0))
+                {
+                    TokenSource.Cancel();
+                    Console.WriteLine($"Cancellation token activated!", Console.ForegroundColor = ConsoleColor.Yellow);
+                    continue;
+                }
+
+                if (filesInQueue.Count != 0 && Semaphore > 0)
+                {
+                    string file;
+                    lock (filesInQueue)
+                    {
+                        Semaphore--;
+                        file = filesInQueue.Dequeue();
+                    }
+                    tasks.Add(Task.Run(() => ProcessFile(file), token));
+                }
+            }
+
+            try
+            {
+                await Task.WhenAll(tasks.ToArray());
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine($"Your processFile tasks were canceled.", Console.ForegroundColor = ConsoleColor.Magenta);
+            }
+            return filesConsumed;
+        }
+
+        private static async Task ProcessFile(string filesPath)
+        {
+            Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+
+            var file = filesPath;
+
+            if (TokenSource.Token.IsCancellationRequested)
+            {
+                Console.WriteLine($"{processName}: Cancel requested for {Path.GetFileName(file)}");
+                return;
+            }
+
+            Console.WriteLine($"{processName}: {Path.GetFileName(file)}", Console.ForegroundColor = ConsoleColor.Red);
+            lock (filesConsumed)
+            {
+                Semaphore++;
+                filesConsumed.Add($"{Path.GetFileName(file)} - Thread id: {Thread.CurrentThread.ManagedThreadId}");
+            }
+
+            TasksCount.Signal();
+        }
+    }
+}
---
+++ FileConsumer/FileConsumerAsync/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using FileGenerator;
+
+namespace FileConsumerAsync
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data");
+            const int fileToGenerate = 15;
+            const int fileToProcess = 10;
+            const int tasksLimit = 4;
+
+            FilesGenerator.Delete(filePath);
+
+            var mainWatcher = new FileConsumerAsync(filePath, fileToProcess, tasksLimit);
+
+            _ = Task.Run(() => FilesGenerator.GenerateFiles(filePath, fileToGenerate));
+
+            var files = await mainWatcher.StartAsync();
+
+            Console.WriteLine("Files processing complete:");
+            foreach (var file in files)
+            {
+                Console.WriteLine($"\t{file}");
+            }
+            Console.WriteLine("End.");
+            Console.ReadLine();
+        }
+    }
+}
